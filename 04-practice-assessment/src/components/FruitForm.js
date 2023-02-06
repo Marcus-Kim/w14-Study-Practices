c5b5e92b94324b849472b2e3c16d82588227a10d
Marcus Kim <kim.marcus97@gmail.com>
2023-02-05 21:41:29 -0700

    Finishe phase 2 pa

--- 04-practice-assessment/src/components/FruitForm.js
+++ 04-practice-assessment/src/components/FruitForm.js
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const COLORS = [
   "red",
   "orange",
@@ -8,24 +12,70 @@ const COLORS = [
 ];
 
 function FruitForm({ fruits }) {
+  //TODO Make name, sweetness, seeds, and color inputs into controlled inputs
+  const [name, setName] = useState('');
+  const [sweetness, setSweetness] = useState(1);
+  const [seeds, setSeeds] = useState('yes');
+  const [color, setColor] = useState(COLORS[0]);
+  const [errors, setErrors] = useState([]);
+  const history = useHistory();
+
+  useEffect(() => { // Validations
+    const errors = [];
+    // if the name is less than 3 characters
+    if (name.length < 3) errors.push('Name must be 3 or more characters');
+    // if the name is more than 20 characters
+    if (name.length > 20) errors.push('Name must be 20 characters or less');
+    // if the name already exists
+    fruits.forEach(fruit => {
+      if (fruit.name === name) errors.push('Name already exists');
+    });
+    // if the sweetness is greater than 10 or less than 1
+    if (sweetness > 10 || sweetness < 1) errors.push('Sweetness must be between 1 and 10');
+
+    // set the errors state to the new errors array with populated errors
+    setErrors(errors);
+  }, [name, sweetness, fruits])
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    
+    const formValues = {
+      name,
+      sweetness,
+      color,
+      seeds
+    }
+
+    console.log(formValues);
+    history.push('/')
+  }
 
   return (
     <form
       className="fruit-form"
+      onSubmit={onSubmit}
     >
       <h2>Enter a Fruit</h2>
       <ul className="errors">
+        {errors.map(error => {
+          return <li key={error}>{error}</li>
+        })}
       </ul>
       <label>
         Name
         <input
           type="text"
           name="name"
+          value={name}
+          onChange={e => setName(e.target.value)}
         />
       </label>
       <label>
         Select a Color
         <select
+          value={color}
+          onChange={e => setColor(e.target.value)}
         >
           {COLORS.map(color => (
             <option
@@ -42,6 +92,8 @@ function FruitForm({ fruits }) {
         <input
           type="number"
           name="sweetness"
+          value={sweetness}
+          onChange={e => setSweetness(e.target.value)}
         />
       </label>
       <label>
@@ -49,6 +101,8 @@ function FruitForm({ fruits }) {
           type="radio"
           value="no"
           name="seeds"
+          checked={seeds === "no"}
+          onChange={e => setSeeds(e.target.value)}
         />
         No Seeds
       </label>
@@ -57,11 +111,14 @@ function FruitForm({ fruits }) {
           type="radio"
           value="yes"
           name="seeds"
+          checked={seeds === "yes"}
+          onChange={e => setSeeds(e.target.value)}
         />
         Seeds
       </label>
       <button
         type="submit"
+        disabled={errors.length ? true : false}
       >
         Submit Fruit
       </button>
--- 04-practice-assessment/src/components/FruitsIndex.js
+++ 04-practice-assessment/src/components/FruitsIndex.js
@@ -1,5 +1,15 @@
-function FruitsIndex() {
-  return null;
+import { Link } from "react-router-dom";
+
+
+function FruitsIndex({ fruits }) {
+  return (
+    <div className="fruits-index">
+      <h2>Fruits Index</h2>
+      {fruits.map(fruit => {
+        return <Link key={fruit.id} to={`/fruits/${fruit.id}`}>{fruit.name}</Link>
+      })}
+    </div>
+  )
 }
 
 export default FruitsIndex;
